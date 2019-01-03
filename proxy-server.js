85e987c934b2e30254896c793eefc27d35670921
Michael Kardjian <makardjian@gmail.com>
2019-01-02 16:13:03 -0800

    debug my proxy server

--- proxy-server.js
+++ proxy-server.js
@@ -5,8 +5,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/:productId', express.static(path.join(__dirname, './public/')));
+
+
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
+
+
